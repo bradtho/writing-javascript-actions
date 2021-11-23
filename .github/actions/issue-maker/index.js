8e7ba4607337a1192f6e9dd492599f29d0a1e53b
Brad Thomas <brad.thomas@azenix.com.au>
2021-11-24 10:31:18 +1100

    Updating octokit

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -9,14 +9,14 @@ async function run() {
 
         const octokit = github.getOctokit(token);
 
-        const newIssue = await octokit.issues.create({
+        const newIssue = await octokit.rest.issues.create({
             repo: github.context.repo.repo,
             owner: github.context.repo.owner,
             title: issueTitle,
             body: jokeBody
         });
-    } catch (err) {
-        core.setFailed(err.message);
+    } catch (error) {
+        core.setFailed(error.message);
     }
 }
 
